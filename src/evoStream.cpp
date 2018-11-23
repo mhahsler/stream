a7f60ab681c835881a64fab4573d4735027f4674
Matthias Carnein <Matthias.Carnein@uni-muenster.de>
2018-11-23 13:31:50 +0100

    Improved evoStream documentation and interfaces

--- src/evoStream.cpp
+++ src/evoStream.cpp
@@ -98,13 +98,13 @@ public:
 
 
   // since exposed constructors have a limited number of parameters, we expose a setter function and use the default constructor
-  void setFields(double r, double lambda, int tgap, int incrementalGenerations, int reclusterGenerations, unsigned int k, double crossoverRate, double mutationRate, int populationSize, unsigned int initializeAfter){
+  void setFields(double r, double lambda, int tgap, unsigned int k, int incrementalGenerations, int reclusterGenerations, double crossoverRate, double mutationRate, int populationSize, unsigned int initializeAfter){
     this->r=r;
     this->lambda=lambda;
     this->tgap=tgap;
+    this->k=k;
     this->incrementalGenerations = incrementalGenerations;
     this->reclusterGenerations = reclusterGenerations;
-    this->k=k;
     this->crossoverRate=crossoverRate;
     this->mutationRate=mutationRate;
     this->populationSize=populationSize;
@@ -380,6 +380,7 @@ public:
 
 
   void evolution(){
+    if(!this->init) return;
 
     this->calculateFitness();
 
@@ -428,7 +429,7 @@ public:
 
 
   void recluster(int generations){
-    Rcpp::NumericMatrix microCluster = this->get_microclusters();
+    if(!this->init) return;
 
     // fixed number of generations
     for(int i=0; i<generations; i++){
@@ -466,7 +467,7 @@ public:
 
     // calc distance to centers
     for(int i=0; i<assignment.size(); i++){
-      result += pow(this->micro[i].distance(centres(assignment[i],_)),2);
+      result += pow(this->micro[i].distance(centres(assignment[i],_)),2) * this->micro[i].weight;
     }
 
     return(1/result);
