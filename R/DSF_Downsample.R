5b46b15dcdec783625cf927e7d29ddeefcf24b22
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-06-21 12:51:12 +0200

    refactor: use more deparse1 in other places where a string is expected

--- R/DSF_Downsample.R
+++ R/DSF_Downsample.R
@@ -86,7 +86,7 @@ update.DSF_Downsample <- function(object,
   if (is.null(dsd))
     dsd <- object$dsd
   if (is.null(dsd))
-    stop("No dsd specified in ", deparse(substitute(object)), ". Specify a dsd in update().")
+    stop("No dsd specified in ", deparse1(substitute(object)), ". Specify a dsd in update().")
 
   if (n == 0)
     return(get_points(dsd, n = 0L, info = info))
